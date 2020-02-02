9c83cedb66397c27ec554e38e29f3e5b44770492
sc11p <evtmail@gmail.com>
2020-02-03 01:11:20 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  const items = ['Learn react', 'Buaild app'];
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+    </ul>
+  )
+};
+
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  )
+};
+
+const SearchPanel = () => {
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '25px'
+  };
+
+  return <input 
+  style = { searchStyle }
+  placeholder= { searchText } />
+};
+
+
+const App = () => {
+  
+  const isLoggedIn = false;  
+  const loginBox = <span>Login please</span>;
+  const welcomeBox = <span>Welcome back</span>;
+
+  return (
+    <div>
+      { isLoggedIn ? welcomeBox : loginBox }
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
